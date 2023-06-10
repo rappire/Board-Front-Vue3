d3a121d9b5a96cf9aa961ebb791be6c4253f3125
rappire <rappire@gmail.com>
2023-06-10 16:29:47 +0900

    feature: delete modal

---
+++ src/components/Delete/DeleteModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>글 삭제</h1>
+    <form @submit.prevent="deleteArticle">
+      <label for="password">비밀번호:</label>
+      <input type="password" v-model="password" required />
+      <button type="submit">삭제</button>
+      <button type="button" @click="cancel">취소</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      password: "",
+    };
+  },
+  methods: {
+    deleteArticle() {
+      const id = this.$route.params.id;
+      this.$store.dispatch("deleteArticle", { id: id, password: this.password });
+    },
+    cancel() {
+      this.$router.back();
+    },
+  },
+};
+</script>
+
+<style>
+/* 삭제 페이지의 스타일 */
+</style>
---
+++ src/view/BoardDelete.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <DeleteModalVue />
+  </div>
+</template>
+
+<script>
+import DeleteModalVue from "@/components/Delete/DeleteModal.vue";
+export default {
+  components: {
+    DeleteModalVue,
+  },
+};
+</script>
+
+<style></style>
